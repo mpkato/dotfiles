663a695520eb2c6008ebd5b124633e6147fb41f9
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2018-05-31 17:27:44 +0900

    Add tmux.conf

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 ln -sf $SCRIPT_DIR/.vimrc $HOME/.vimrc
+ln -sf $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf
+TMUX_ALIAS="alias tmux=\"tmux -2\""
+PROFILE=$HOME/.bash_profile
+grep -qF "$TMUX_ALIAS" $PROFILE || echo $TMUX_ALIAS >> $PROFILE
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 vim +NeoBundleInstall +qall
