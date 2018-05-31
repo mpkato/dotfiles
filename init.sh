c8d5b68f3e86f6b67656b797e9ea29ec95ff0c7a
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2018-05-31 16:32:55 +0900

    Add symlink

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+ln -sf $SCRIPT_DIR/.vimrc $HOME/.vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 vim +NeoBundleInstall +qall
